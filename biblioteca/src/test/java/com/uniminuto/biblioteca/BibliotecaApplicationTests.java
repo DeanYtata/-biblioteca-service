78912251044461622b85f227c61ac60e8a8afb9b
DeanYtata <josephdelvalle0399@gmail.com>
2026-02-21 16:32:47 -0500

    feat: estructura base spring boot

---
+++ biblioteca/src/test/java/com/uniminuto/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniminuto.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
